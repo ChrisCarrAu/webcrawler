46c88883d6e55c84a5e5fcfdbee61d3482c36ef4
Chris Carr <chris@thegravenimage.com>
2018-03-22 08:54:46 +1100

    Moved files back into Implementation & Interface folders

---
+++ CrawlerLib/Service/Implementation/UriQueueListener.cs
@@ -0,0 +1,34 @@
+﻿using Crawler.Lib.Model;
+using Crawler.Lib.Repository.Interface;
+using Crawler.Lib.Service.Interface;
+using System;
+
+namespace Crawler.Lib.Service.Implementation
+{
+    public class UriQueueListener : IUriQueueListener
+    {
+        private readonly IUriQueue _uriQueue;
+        private readonly IProcessedSet _processedSet;
+
+        public UriQueueListener(IUriQueue uriQueue, IProcessedSet processedSet)
+        {
+            _uriQueue = uriQueue;
+            _processedSet = processedSet;
+        }
+
+        public void OnCompleted()
+        {
+            //
+        }
+
+        public void OnError(Exception error)
+        {
+            //
+        }
+
+        public void OnNext(Anchor value)
+        {
+            //
+        }
+    }
+}
---
+++ CrawlerLib/Service/Interface/IUriQueueListener.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Crawler.Lib.Model;
+
+namespace Crawler.Lib.Service.Interface
+{
+    public interface IUriQueueListener : IObserver<Anchor>
+    {
+    }
+}
