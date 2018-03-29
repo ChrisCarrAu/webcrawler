13ba85fa0c549b022b313bb1e57dbe26b882c428
Chris Carr <chris@appthem.com>
2018-03-30 08:00:07 +1100

    Refactored

--- FCrawler/Program.fs
+++ FCrawler/Program.fs
@@ -5,11 +5,14 @@ type SpiderUri =
     { 
         Address : Uri;
         Text : string option;
-        Depth : int32 
+        crawlDepth : int32 
     }
-//    | Address of string
-//    | Depth of int
 
+type SpiderResult =
+    {
+        htmlNode : HtmlNode;
+        href : HtmlAttribute;
+    }
 
 [<EntryPoint>]
 let main args =
@@ -17,23 +20,28 @@ let main args =
     // Example:
     //Helpers.crawl "http://news.google.com" 25
 
-//    let crawledUri baseUri relative text = 
-//        { Address = new Uri(baseUri.Address, relative); Text = Some(text); Depth = baseUri.Depth }
-
     let parseAnchors (uri : SpiderUri) = 
         HtmlDocument.Load(uri.Address.ToString()).Descendants["a"]
+
+    let parseAnchorLinks (anchors : seq<HtmlNode>) =
+        anchors
         |> Seq.choose ( fun htmlNode -> 
             htmlNode.TryGetAttribute("href")
-            |> Option.map (fun a -> { Text = Some(htmlNode.InnerText()); Address = Uri(uri.Address, a.Value()); Depth = uri.Depth + 1 } ) )
+            |> Option.map(fun h -> { htmlNode = htmlNode; href = h } ))
+
+    let spiderUri ( spiderResult ) ( uribase : SpiderUri ) =
+        { Text = Some(spiderResult.htmlNode.InnerText()); Address = Uri(uribase.Address, spiderResult.href.Value()); crawlDepth = uribase.crawlDepth + 1 }
 
     let rec spiderAgent = MailboxProcessor.Start(fun uriQueue->
         let rec crawlLoop() = async {
             let! uri = uriQueue.Receive()
             
-            parseAnchors(uri)
+            parseAnchors(uri) 
+            |> parseAnchorLinks
+            |> Seq.map (fun f -> spiderUri f uri)
             |> Seq.iter (fun f -> 
-                printfn "%A %d" f.Address f.Depth;
-                if (f.Depth < 2) then 
+                printfn "%A %d" f.Address f.crawlDepth;
+                if f.crawlDepth < 2 then 
                     spiderAgent.Post f
                 )
 
@@ -43,7 +51,8 @@ let main args =
         crawlLoop()
     )
 
-    spiderAgent.Post { Address = Uri("http://appthem.com"); Depth = 0; Text = None }
+    spiderAgent.Post { Address = Uri("http://appthem.com"); crawlDepth = 0; Text = None }
+    //spiderAgent.Post { Address = Uri("https://www.bikesales.com.au"); crawlDepth = 0; Text = None }
     
     Console.ReadLine() |> ignore
 
